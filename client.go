dba4abf016a47cd9709175761d1179e462ead67a
Jamie <43911414+jamiewhitney@users.noreply.github.com>
2022-05-16 13:07:54 +0100

    Update client.go

--- client.go
+++ client.go
@@ -4,9 +4,11 @@ import (
 	"crypto/tls"
 	"errors"
 	"fmt"
+	"log"
+
+	"encoding/base64"
 	vault "github.com/hashicorp/vault/api"
 	"github.com/hashicorp/vault/sdk/helper/certutil"
-	"log"
 )
 
 type Client struct {
@@ -26,15 +28,26 @@ type Credential struct {
 	SecretID string
 }
 
+type DatabaseCredentials struct {
+	Username string
+	Password string
+	LeaseID  string
+}
+
+type AWSCredentials struct {
+	Access_key string
+	Secret_key string
+	Secret     *vault.Secret
+}
+
 func NewClient(config *Vault) (*Client, error) {
 	client, err := vault.NewClient(vault.DefaultConfig())
-	//Set the address
+
 	err = client.SetAddress(fmt.Sprintf(config.Address))
 	if err != nil {
 		return nil, err
 	}
 
-	//Auth to Vault
 	log.Println("Client authenticating to Vault")
 	switch config.Authentication {
 	case "token":
@@ -53,17 +66,14 @@ func NewClient(config *Vault) (*Client, error) {
 	case "approle":
 		log.Println("Using approle authentication")
 
-		//Check Mount
 		if len(config.Credential.RoleID) == 0 {
 			return nil, errors.New("Role ID not found.")
 		}
 
-		//Check Mount
 		if len(config.Credential.SecretID) == 0 {
 			return nil, errors.New("Secret ID not found.")
 		}
 
-		//Auth with approle vault
 		data := map[string]interface{}{"role_id": config.Credential.RoleID, "secret_id": config.Credential.SecretID}
 		secret, err := client.Logical().Write(fmt.Sprintf("auth/%s/login", config.Path), data)
 		if err != nil {
@@ -101,9 +111,9 @@ func (c *Client) GetTLSConfig(path string, data map[string]interface{}) (*tls.Co
 
 }
 
-func (c *Client) RenewSecret(secret vault.Secret) error {
+func (c *Client) RenewSecret(secret *vault.Secret) error {
 	watcher, err := c.NewLifetimeWatcher(&vault.LifetimeWatcherInput{
-		Secret: &secret,
+		Secret: secret,
 	})
 	if err != nil {
 		log.Fatal(err)
@@ -123,4 +133,72 @@ func (c *Client) RenewSecret(secret vault.Secret) error {
 			log.Printf("Succesfully renewed secret %s", watcher.Secret.LeaseID)
 		}
 	}
+}
+
+func (c *Client) GetSecret(path string) (*vault.Secret, error) {
+	log.Printf("Getting secret: %s", path)
+	secret, err := c.Logical().Read(path)
+	if err != nil {
+		return secret, nil
+	}
+	return secret, nil
+}
+
+func (c *Client) GetDatabaseCredentials(path string) (*DatabaseCredentials, error) {
+	credentials, err := c.Logical().Read(path)
+	if err != nil {
+		return nil, err
+	}
+
+	go c.RenewSecret(credentials)
+
+	return &DatabaseCredentials{
+		Username: credentials.Data["username"].(string),
+		Password: credentials.Data["password"].(string),
+		LeaseID:  credentials.LeaseID,
+	}, nil
+}
+
+func (c *Client) Encrypt(path string, plaintext string) (string, error) {
+	encoded := base64.StdEncoding.EncodeToString([]byte(plaintext))
+	data := map[string]interface{}{"plaintext": encoded}
+
+	encrypted, err := c.Client.Logical().Write(path, data)
+	if err != nil {
+		return "", err
+	}
+
+	ciphertext := encrypted.Data["ciphertext"].(string)
+
+	return ciphertext, nil
+}
+
+func (c *Client) Decrypt(path string, ciphertext string) (string, error) {
+	decrypted, err := c.Logical().Write(path, map[string]interface{}{
+		"ciphertext": ciphertext,
+	})
+	if err != nil {
+		return "", err
+	}
+
+	decryptedData := decrypted.Data["plaintext"].(string)
+
+	decoded, _ := base64.StdEncoding.DecodeString(decryptedData)
+
+	return string(decoded), nil
+}
+
+func (c *Client) GetAWSCredentials(path string) (*AWSCredentials, error) {
+	credentials, err := c.Logical().Read(path)
+	if err != nil {
+		return nil, err
+	}
+
+	go c.RenewSecret(credentials)
+
+	return &AWSCredentials{
+		Access_key: credentials.Data["access_key"].(string),
+		Secret_key: credentials.Data["secret_key"].(string),
+		Secret:     credentials,
+	}, nil
 }
